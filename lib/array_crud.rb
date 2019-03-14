cb4365c95b763613513a7d2df26b434f16cd2de7
Dennis Chung <chungdennis6@gmail.com>
2019-03-14 04:10:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["France", "Germany", "Russia", "England"]
   array.push("Austria")
+  puts array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
